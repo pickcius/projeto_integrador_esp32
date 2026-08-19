76054987e63be6dd4b9af94f5f75dfad06b1ae60
Gabriel <150564062+pickcius@users.noreply.github.com>
2026-08-19 18:02:03 -0300

    tentando-cadastro

--- AutomacaoResidencial/usuarios/views.py
+++ AutomacaoResidencial/usuarios/views.py
@@ -1,4 +1,68 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def login(request):
-    return render(request, "usuarios/login.html")
+    if request.method == 'POST':
+
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+
+        usuario = authenticate(
+            request,
+            username=email,
+            password=senha
+        )
+
+        if usuario is not None:
+
+            login(request, usuario)
+
+            return redirect('dashboard')
+
+        else:
+
+            messages.error(
+                request,
+                'E-mail ou senha incorretos.'
+            )
+
+            return redirect('login')
+
+    return render(request, 'login.html')
+
+def cadastro(request):
+      if request.method == 'POST':
+
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        # Verifica se as senhas são iguais
+        if senha != confirmar_senha:
+            messages.error(request, 'As senhas não coincidem.')
+            return redirect('cadastro')
+
+        # Verifica se o e-mail já está cadastrado
+        if User.objects.filter(email=email).exists():
+            messages.error(request, 'Este e-mail já está cadastrado.')
+            return redirect('cadastro')
+
+        # Cria o usuário
+        usuario = User.objects.create_user(
+            username=email,
+            email=email,
+            password=senha,
+            first_name=nome
+        )
+
+        usuario.save()
+
+        messages.success(request, 'Cadastro realizado com sucesso!')
+
+        return redirect('login')
+
+    return render(request, 'cadastro.html')
